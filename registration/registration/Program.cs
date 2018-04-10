ff45bdba3f589bbd1ce8a911f576483706c15934
Qasimovrufat <34399399+Qasimovrufat@users.noreply.github.com>
2018-04-10 20:43:30 +0400

    Add files via upload

---
+++ registration/registration/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace registration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            {
+                Console.WriteLine("QEYDIYYAT");
+                Console.WriteLine("---------------------");
+                user u = new user();
+                Console.WriteLine("ISTIFADECI ADI");
+                u.Username = Console.ReadLine();
+                Console.WriteLine("SIFRE");
+                u.Password = Console.ReadLine();
+              
+                
+              
+            
+                
+               
+                Console.WriteLine("---------------------");
+                int count = 0;
+                string user_enter;
+                string pass_enter;
+
+
+                while(true)
+                {
+                    Console.WriteLine(" HESABA DAXIL OLUN:");
+                    Console.WriteLine("---------------------");
+                    Console.WriteLine(" MOVCUD ISTIFADECI ADI:");
+                    user_enter = Console.ReadLine();
+                    Console.WriteLine("MOVCUD SIFRE:");
+                    pass_enter = Console.ReadLine();
+                    Console.WriteLine("---------------------");
+
+                    if (u.Username == user_enter && u.Password == pass_enter)
+                    {
+                        Console.WriteLine("SISTEME GIRIS ETDINIZ");
+                        break;
+                    }
+                    if (u.Username != user_enter || u.Password != pass_enter)
+                    {
+                        
+                        count++;
+                        if (count == 3)
+                        {
+                            Console.WriteLine("HESABINIZ BLOKLANDI");
+                            break;
+                        }
+                        Console.WriteLine("SIFRE VE YA AD YANLISDIR YENIDEN YOXLAYIN");
+                    }
+
+                }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            }
+        }
+    }
+}
---
+++ registration/registration/user.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace registration
+{
+    class user
+    {
+        private string _username;
+        private string _password;
+
+        public string Username{
+            get
+            {
+                return this._username;
+            }
+            set
+            {
+                this._username = value;
+            }
+        }
+        public string Password
+        {
+            get
+            {
+                return this._password;
+            }
+            set
+            {
+                this._password = value;
+            }
+        }
+
+
+    }
+}
